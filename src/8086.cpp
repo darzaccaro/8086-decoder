d5b12c39f9ad7c601453b5653e99bac2d679f4d5
Dar Zaccaro <darzaccaro@gmail.com>
2023-03-10 12:49:26 -0500

    handle more movs

--- src/8086.cpp
+++ src/8086.cpp
@@ -25,6 +25,7 @@
 #define MOD_MASK 0b11000000
 
 void decode(cstring inputName);
+std::string get_register_string(u8 byte, bool is16Bit);
 
 int main() {
     using std::string;
@@ -42,16 +43,17 @@ int main() {
 
 void decode(cstring inputName) {
     auto input = new File(std::format("test-data/computer_enhance/perfaware/part1/{}", inputName).c_str());
-    auto reference = new File(std::format("test-data/computer_enhance/perfaware/part1/{}.asm", inputName).c_str());
+    // TODO diff the output against the reference
+    // auto reference = new File(std::format("test-data/computer_enhance/perfaware/part1/{}.asm", inputName).c_str());
     std::string output = std::format(";{} decoded\n\nbits 16\n\n", inputName);
     u8* ptr = input->data;
     while (ptr < input->data + input->size) {
         u8 byte1 = *ptr;
         ptr++;
         if (OP_MOVE_REGISTER_OR_MEMORY_TO_OR_FROM_REGISTER(byte1)) {
+            std::string reg, rm, source, dest;
             u8 byte2 = *ptr;
             ptr++;
-            std::string reg, rm, source, dest;
             switch (byte2 & MOD_MASK) {
             case MOD_MEMORY_MODE_NO_DISPLACEMENT:
             {
@@ -65,78 +67,63 @@ void decode(cstring inputName) {
                 case 0b00000101: rm = "di"; break;
                 case 0b00000110:
                 {
-                    u8 byte3 = *ptr;
-                    ptr++;
-                    u8 byte4 = *ptr;
-                    ptr++;
-                    rm = std::format("[{}]", byte3 + byte4);
+                    u16 displacement = *((u16*)ptr);
+                    ptr += 2;
+                    rm = std::format("[{}]", displacement);
                 } break;
                 case 0b00000111: rm = "bx"; break;
                 }
             } break;
             case MOD_MEMORY_MODE_8_BIT_DISPLACEMENT:
             {
-                u8 byte3 = *ptr;
+                u8 displacement = *ptr;
                 ptr++;
                 // r/m mask
                 switch (byte2 & 0b00000111) {
-                case 0b00000000: rm = std::format("[bx + si + {}]", byte3); break;
-                case 0b00000001: rm = std::format("[bx + di + {}]", byte3); break;
-                case 0b00000010: rm = std::format("[bp + si + {}]", byte3); break;
-                case 0b00000011: rm = std::format("[bp + di + {}]", byte3); break;
-                case 0b00000100: rm = std::format("[si + {}]", byte3); break;
-                case 0b00000101: rm = std::format("[di + {}]", byte3); break;
-                case 0b00000110: rm = std::format("[bp + {}]", byte3); break;
-                case 0b00000111: rm = std::format("[bx + {}]", byte3); break;
-                } break;
+                case 0b00000000: rm = std::string("[bx + si"); break;
+                case 0b00000001: rm = std::string("[bx + di"); break;
+                case 0b00000010: rm = std::string("[bp + si"); break;
+                case 0b00000011: rm = std::string("[bp + di"); break;
+                case 0b00000100: rm = std::string("[si"); break;
+                case 0b00000101: rm = std::string("[di"); break;
+                case 0b00000110: rm = std::string("[bp"); break;
+                case 0b00000111: rm = std::string("[bx"); break;
+                }
+                if (displacement != 0) {
+                    rm += std::format(" + {}]", displacement);
+                } else {
+                    rm += ']';
+                }
             } break;
             case MOD_MEMORY_MODE_16_BIT_DISPLACEMENT:
             {
-                u8 byte3 = *ptr;
-                ptr++;
-                u8 byte4 = *ptr;
-                ptr++;
+                u16 displacement = *((u16*)ptr);
+                ptr += 2;
                 // r/m mask
                 switch (byte2 & 0b00000111) {
-                case 0b00000000: rm = std::format("[bx + si + {}]", byte3 + byte4); break;
-                case 0b00000001: rm = std::format("[bx + di + {}]", byte3 + byte4); break;
-                case 0b00000010: rm = std::format("[bp + si + {}]", byte3 + byte4); break;
-                case 0b00000011: rm = std::format("[bp + di + {}]", byte3 + byte4); break;
-                case 0b00000100: rm = std::format("[si + {}]", byte3 + byte4); break;
-                case 0b00000101: rm = std::format("[di + {}]", byte3 + byte4); break;
-                case 0b00000110: rm = std::format("[bp + {}]", byte3 + byte4); break;
-                case 0b00000111: rm = std::format("[bx + {}]", byte3 + byte4); break;
-                } break;
+                case 0b00000000: rm = std::string("[bx + si"); break;
+                case 0b00000001: rm = std::string("[bx + di"); break;
+                case 0b00000010: rm = std::string("[bp + si"); break;
+                case 0b00000011: rm = std::string("[bp + di"); break;
+                case 0b00000100: rm = std::string("[si"); break;
+                case 0b00000101: rm = std::string("[di"); break;
+                case 0b00000110: rm = std::string("[bp"); break;
+                case 0b00000111: rm = std::string("[bx"); break;
+                }
+                if (displacement != 0) {
+                    rm += std::format(" + {}]", displacement);
+                } else {
+                    rm += ']';
+                }
             } break;
             case MOD_REGISTER_MODE_NO_DISPLACEMENT:
             {
                 // r/m mask
-                switch (byte2 & 0b00000111) {
-                //                      check w bit to determine if register is 8 or 16 bits
-                case 0b00000000: rm = ((byte1 & 0b00000001) > 0) ? "ax" : "al"; break;
-                case 0b00000001: rm = ((byte1 & 0b00000001) > 0) ? "cx" : "cl"; break;
-                case 0b00000010: rm = ((byte1 & 0b00000001) > 0) ? "dx" : "dl"; break;
-                case 0b00000011: rm = ((byte1 & 0b00000001) > 0) ? "bx" : "bl"; break;
-                case 0b00000100: rm = ((byte1 & 0b00000001) > 0) ? "sp" : "ah"; break;
-                case 0b00000101: rm = ((byte1 & 0b00000001) > 0) ? "bp" : "ch"; break;
-                case 0b00000110: rm = ((byte1 & 0b00000001) > 0) ? "si" : "dh"; break;
-                case 0b00000111: rm = ((byte1 & 0b00000001) > 0) ? "di" : "bh"; break;
-                }
+                rm = get_register_string(byte2, (byte1 & 0b00000001) > 0);
             } break;
             }
             // reg mask
-            switch (byte2 & 0b00111000) {
-            //                       check w bit to determine if register is 8 or 16 bits
-            case 0b00000000: reg = ((byte1 & 0b00000001) > 0) ? "ax" : "al"; break;
-            case 0b00001000: reg = ((byte1 & 0b00000001) > 0) ? "cx" : "cl"; break;
-            case 0b00010000: reg = ((byte1 & 0b00000001) > 0) ? "dx" : "dl"; break;
-            case 0b00011000: reg = ((byte1 & 0b00000001) > 0) ? "bx" : "bl"; break;
-            case 0b00100000: reg = ((byte1 & 0b00000001) > 0) ? "sp" : "ah"; break;
-            case 0b00101000: reg = ((byte1 & 0b00000001) > 0) ? "bp" : "ch"; break;
-            case 0b00110000: reg = ((byte1 & 0b00000001) > 0) ? "si" : "dh"; break;
-            case 0b00111000: reg = ((byte1 & 0b00000001) > 0) ? "di" : "bh"; break;
-            }
-            
+            reg = get_register_string(byte2 >> 3, (byte1 & 0b00000001) > 0);
             // check d bit to determine which registers are source and destination
             if ((byte1 & 0b00000010) > 0) {
                 source = rm;
@@ -146,16 +133,54 @@ void decode(cstring inputName) {
                 dest = rm;
             }
             output += std::format("mov {}, {}\n", dest, source);
+#if 0
         } else if (OP_MOVE_IMMEDIATE_TO_REGISTER_OR_MEMORY(byte1)) {
-            OutputDebugString("unimplemented OPCODE_MASK_IMMEDIATE_TO_REGISTER_OR_MEMORY\n");
-            ASSERT(false);
+            //           w mask
+            if ((byte1 & 0b00000001) > 0) {
+            } else {
+                    
+            }
+#endif
         } else if (OP_MOVE_IMMEDIATE_TO_REGISTER(byte1)) {
-            OutputDebugString("unimplemented OPCODE_MASK_IMMEDIATE_TO_REGISTER\n");
-            ASSERT(false);
+            std::string reg, rm, source, dest;
+            //OutputDebugString("unimplemented OPCODE_MASK_IMMEDIATE_TO_REGISTER\n");
+            //ASSERT(false);
+            //           w mask
+            if ((byte1 & 0b00001000) > 0) {
+                // instruction is 3 bytes (2 bytes of data)
+                reg = get_register_string(byte1, true);
+                dest = reg;
+                i16 immediate = *((u16*)ptr);
+                ptr += 2;
+                source = std::format("{}", immediate);
+            } else {
+                // instruction is 2 bytes (1 byte of data)
+                reg = get_register_string(byte1, false);
+                dest = reg;
+                i8 immediate = *((i8*)ptr);
+                ptr++;
+                source = std::format("{}", immediate);
+            }
+            output += std::format("mov {}, {}\n", dest, source);
         } else {
             OutputDebugString("encountered unimplemented opcode\n");
             ASSERT(false);
         }
     }
     File::write(std::format("test-data/output/{}-output.asm", inputName).c_str(), output);
+}
+
+std::string get_register_string(u8 byte, bool is16Bit) {
+    ASSERT((byte & 0b00000111) <= 0b00000111);
+    switch (byte & 0b00000111) {
+    case 0b00000000: return (is16Bit) ? "ax" : "al";
+    case 0b00000001: return (is16Bit) ? "cx" : "cl";
+    case 0b00000010: return (is16Bit) ? "dx" : "dl";
+    case 0b00000011: return (is16Bit) ? "bx" : "bl";
+    case 0b00000100: return (is16Bit) ? "sp" : "ah";
+    case 0b00000101: return (is16Bit) ? "bp" : "ch";
+    case 0b00000110: return (is16Bit) ? "si" : "dh";
+    case 0b00000111: return (is16Bit) ? "di" : "bh";
+    default: ASSERT(false); return "";
+    }
 }
